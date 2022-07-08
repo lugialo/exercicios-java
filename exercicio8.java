1df68f3c5a65a338a5a88da93b4d3e6ffd38062f
Gabriel Antonin <103425929+lugialo@users.noreply.github.com>
2022-07-08 19:38:30 -0300

    Update exercicio8.java

--- exercicio8.java
+++ exercicio8.java
@@ -2,7 +2,8 @@ import javax.swing.JOptionPane;
 import java.util.Scanner;
 
 public class exercicio8{
-public static void main(String[] args) {
+public static void main(String[] args) 
+{
     Scanner entrada = new Scanner(System.in);
     
     int idade;
@@ -10,22 +11,23 @@ public static void main(String[] args) {
     System.out.println("Digite o valor da sua idade.");
     idade = entrada.nextInt();
 
-    if (idade <= 0 and idade >= 3){
+    if (idade <= 0) && (idade >= 3)
+    {    
         JOptionPane.showMessageDialog(null, "Você é um bebê!"); 
-            }
-    else if (idade <= 4 and idade >=9){
+    }
+    else if (idade <= 4) && (idade >=9){
         JOptionPane.showMessageDialog(null, "Você é uma criança!"); 
-}
-    else if (idade <= 10 and idade >= 13){
+    }
+    else if (idade <= 10) && (idade >= 13){
         JOptionPane.showMessageDialog(null, "Você é um pré-adolescente"); 
     }
-    else if (idade <= 14 and idade >= 17){
+    else if (idade <= 14) && (idade >= 17){
         JOptionPane.showMessageDialog(null, "Você é um adolescente!"); 
     }
-    else if (idade <=18 and idade >= 24){
+    else if (idade <=18) && (idade >= 24){
         JOptionPane.showMessageDialog(null, "Você é um jovem!"); 
     }
-    else if (idade <= 25 and idade >= 50){
+    else if (idade <= 25) && (idade >= 50){
         JOptionPane.showMessageDialog(null, "Você é um adulto!"); 
     }
     else if (idade >= 51){
