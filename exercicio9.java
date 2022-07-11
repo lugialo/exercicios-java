2697f243a641305f971a526339cffc5f08406205
Gabriel Antonin <103425929+lugialo@users.noreply.github.com>
2022-07-11 11:26:59 -0300

    Update exercicio9.java

--- exercicio9.java
+++ exercicio9.java
@@ -11,26 +11,27 @@ public static void main(String[] args)
     System.out.println("Digite o valor da sua idade.");
     idade = entrada.nextInt();
 
-    if (idade <= 0) && (idade >= 3)
+    if (idade <= 0 && idade >= 3)
     {    
         JOptionPane.showMessageDialog(null, "Você é um bebê!"); 
     }
-    else if (idade <= 4) && (idade >=9){
+    else if (idade <= 4 && idade >=9){
         JOptionPane.showMessageDialog(null, "Você é uma criança!"); 
     }
-    else if (idade <= 10) && (idade >= 13){
+    else if (idade <= 10 && idade >= 13){
         JOptionPane.showMessageDialog(null, "Você é um pré-adolescente"); 
     }
-    else if (idade <= 14) && (idade >= 17){
+    else if (idade <= 14 && idade >= 17){
         JOptionPane.showMessageDialog(null, "Você é um adolescente!"); 
     }
-    else if (idade <=18) && (idade >= 24){
+    else if (idade <=18 && idade >= 24){
         JOptionPane.showMessageDialog(null, "Você é um jovem!"); 
     }
-    else if (idade <= 25) && (idade >= 50){
+    else if (idade <= 25 && idade >= 50){
         JOptionPane.showMessageDialog(null, "Você é um adulto!"); 
     }
     else if (idade >= 51){
         JOptionPane.showMessageDialog(null, "Você é um idoso!"); 
     } 
+}
 }
