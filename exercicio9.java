15ea295d30bb30b66272536d66bf48de599b0afe
Gabriel Antonin <103425929+lugialo@users.noreply.github.com>
2022-07-11 11:20:46 -0300

    Update and rename exercicio8.java to exercicio9.java

--- exercicio9.java
+++ exercicio9.java
@@ -1,7 +1,7 @@
 import javax.swing.JOptionPane;
 import java.util.Scanner;
 
-public class exercicio8{
+public class exercicio9{
 public static void main(String[] args) 
 {
     Scanner entrada = new Scanner(System.in);
