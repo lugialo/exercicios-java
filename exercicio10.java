f64132e983781ff73c52598f8f56c3ce9c8e73a8
Gabriel Antonin <103425929+lugialo@users.noreply.github.com>
2022-07-16 00:48:16 -0300

    Update exercicio10.java

--- exercicio10.java
+++ exercicio10.java
@@ -1,28 +1,54 @@
+
 import javax.swing.JOptionPane;
 
 public class exercicio10 {
 
     public static void main(String[] args) {
-        int idade;
+        
+    
         String aux="";
-        float nota1=0, nota2=0, trabalho=0, media=0;
+        float peso=0;
+        float imc=0;
+        float altura=0;
         try{
-            aux = JOptionPane.showInputDialog(null, "Nota 1");
-            nota1 = Float.parseFloat(aux);
+            aux = JOptionPane.showInputDialog(null, "Digite seu peso (em kg!)");
+            peso = Float.parseFloat(aux);
 
-            aux = JOptionPane.showInputDialog(null, "Nota 2");
-            nota2 = Float.parseFloat(aux);
+            aux = JOptionPane.showInputDialog(null, "Digite sua altura (em metros!)");
+            altura = Float.parseFloat(aux);
 
-            aux = JOptionPane.showInputDialog(null, "Nota trabalho");
-            trabalho = Float.parseFloat(aux);
+            
 
-            media = (nota1+nota2+trabalho)/3;
-            JOptionPane.showMessageDialog(null, "ahahaa teste " + media);
-
+            imc = (peso) / (altura * altura);
+            
+            if (imc < 18.5)
+            {    
+                JOptionPane.showMessageDialog(null, "Seu IMC é "+ imc + " e a categoria é 'Abaixo do normal'!"); 
+            }
+            else if (imc >= 18.5 && imc <= 24.4)
+            {    
+                JOptionPane.showMessageDialog(null, "Seu IMC é "+ imc + " e a categoria é 'Peso ideal'!"); 
+            }
+            else if (imc >= 24.5 && imc <= 29.9)
+            {    
+                JOptionPane.showMessageDialog(null, "Seu IMC é "+ imc + " e a categoria é 'Pré-obesidade'!"); 
+            }
+            else if (imc >= 30 && imc <= 34.9)
+            {    
+                JOptionPane.showMessageDialog(null, "Seu IMC é "+ imc + " e a categoria é 'Obesidade classe I'!"); 
+            }
+            else if (imc >= 35 && imc <= 39.9)
+            {    
+                JOptionPane.showMessageDialog(null, "Seu IMC é "+ imc + " e a categoria é 'Obesidade severa'!"); 
+            }
+            else if (imc > 39.9)
+            {    
+                JOptionPane.showMessageDialog(null, "Seu IMC é "+ imc + " e a categoria é 'Obesidade mórbida'!"); 
+            }
 
         }
         catch (NumberFormatException erro){
-            JOptionPane.showMessageDialog(null, "ahahahtqetewtwe");
+            JOptionPane.showMessageDialog(null, "Erro");
         }
 
 
@@ -30,5 +56,5 @@ public class exercicio10 {
 
 
     }
-    
+ 
 }
